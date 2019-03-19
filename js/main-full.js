d9d98d0626d9184138d8a77f87a36cd67c16c59f
Markshall <markeriksson94@live.co.uk>
2019-03-19 17:35:44 +0000

    Remove useless JavaScript, tidy up CSS and other various changes to improve the site

--- js/main-full.js
+++ js/main-full.js
@@ -1,6 +1,5 @@
 $(function() {
-	$('header#home').css({'height': $(window).height()});
-	$('#banner h1').fitText(/*.23,{minFontSize: '20px', maxFontSize: '90px'}*/);
+	$('header#home').css('height', $(window).height());
 	
 	$(window).on('resize orientationchange', function() {
 		$('header#home').css('height', $(window).height());
@@ -21,15 +20,9 @@ $(function() {
 		}
 		
 		//animate in my face
-		var me      = $('.profile-picture'),
-			aboutme = $('#about');
-		if (!Modernizr.touch) {
-			if (ScrTop > $('#about').offset().top-350) {
-				me.addClass('fademein');
-			}
-		} else {
-			me.css('opacity', '1');
-		}
+    if (ScrTop > $('#about').offset().top-650) {
+      $('.profile-picture').addClass('fademein');
+    }
 	});
 	
 	$('a.toggleNavigation').on('click', function(e) {
@@ -52,11 +45,11 @@ $(function() {
 		e.preventDefault();
 		
 		var formName    = $('#contactName').val(),
-			formEmail   = $('#contactEmail').val(),
-			formSubject = $('#contactSubject').val(),
-			formContent = $('#contactContent').val(),
-			formError   = '',
-			emailFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,6})+$/;
+        formEmail   = $('#contactEmail').val(),
+        formSubject = $('#contactSubject').val(),
+        formContent = $('#contactContent').val(),
+        formError   = '',
+        emailFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,6})+$/;
 		
 		if (formName.length < 1 || formEmail.length < 1 || formSubject.length < 1 || formContent.length < 1) {
 			formError = 'All fields are required.';
@@ -143,49 +136,20 @@ $(function() {
 	$('.contact-row .input-group input, .contact-row .input-group textarea').focusout(function() {
 		var selfVal = $(this).val();
 		
-		if (selfVal != '') {
-			$(this).addClass('contains-contents');
-		} else {
-			$(this).removeClass('contains-contents');
-		}
-	});
-	
-	$('.project').on('click', function(e) {
-		var self = $(this);
-		
-		$.getJSON('projects.json').done(function(data) {
-			$('.modal-overlay').removeClass('hide');
-			$('.modal-window').removeClass('hide').addClass('zoomIn animated');
-		});
-		
-		e.preventDefault();
-	});
-	
-	$('.modal-overlay, .modal-close').on('click', function(e) {
-		$('.modal-overlay').addClass('hide');
-		$('.modal-window').removeClass('zoomIn').addClass('zoomOut').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(z) {
-			$(this).removeClass('animated zoomOut').addClass('hide');
-		});
-		
-		e.preventDefault();
+    $(this)[
+      (selfVal !== '') ? 'addClass' : 'removeClass'
+    ]('contains-contents');
 	});
 	
 	var waypoints = $("section").waypoint(function(d) {
-			var $activeSection = $(this.element);
-			
-			if (d === 'up')
-				$activeSection = $activeSection.prev();
-			
-			var $activeLink = $('nav#navigation ul li a[href="#' + $activeSection.attr('id') + '"]');
-			//console.log($activeSection, $activeLink);
-			$('nav#navigation ul li').removeClass('active');
-			$activeLink.parent().addClass('active');
-		}, {
-			offset: '25px'
-		}
-	);
-	
-	function DisplayCaseStudy(data) {
+		var $activeSection = $(this.element);
 		
-	}
+		if (d === 'up')
+		  $activeSection = $activeSection.prev();
+		
+		var $activeLink = $('nav#navigation ul li a[href="#' + $activeSection.attr('id') + '"]');
+    
+    $('nav#navigation ul li').removeClass('active');
+		$activeLink.parent().addClass('active');
+  }, {offset: '25px'});
 });
