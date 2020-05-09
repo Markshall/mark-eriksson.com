585cd9600b1af0e1604967e157b75f5701113e96
Markshall <markeriksson94@live.co.uk>
2020-05-09 15:21:14 +0100

    update failure message for email submissions

--- inc/contactSend.php
+++ inc/contactSend.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$email   = htmlspecialchars($_POST['contactEmail']);
 	$subject = htmlspecialchars($_POST['contactSubject']);
 	$msg     = nl2br(htmlspecialchars($_POST['contactContent']));
-	
+
 	if (isset($_POST['contactTel']) && strlen($_POST['contactTel']) > 0) {
 		$result['error'] = 'No bots allowed.';
 	} else {
@@ -22,19 +22,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 					//start email
 					$to = 'markeriksson94@live.co.uk';
 					$headers = "From: contact@mark-eriksson.com\r\nReply-To: {$email}\r\nMIME-Version: 1.0\r\nContent-Type: text/html; charset=ISO-8859-1\r\n";
-					
+
 					$message = "<table border=\"1\"><tbody><tr><td><strong>Name:</strong></td><td>{$name}</td></tr><tr><td><strong>Email:</strong></td><td>{$email}</td></tr><tr><td><strong>Date sent:</strong></td><td>" . date ("jS F Y - g:i a") . "</td></tr><tr><td><strong>IP Address:</strong></td><td>{$_SERVER['REMOTE_ADDR']}</td></tr><tr><td colspan=\"2\"><strong>Message:</strong><br /><br />{$msg}</td></tr></tbody></table>";
-					
+
 					if (!@mail($to, $subject, $message, $headers)) {
-						$result['error'] = 'Your email could not be sent at this time.';
-					} else {						
+						$result['error'] = 'Your email could not be sent at this time. Contact me directly at markeriksson94@live.co.uk';
+					} else {
 						$result['success'] = 'Yes';
 						$result['name'] = $name;
 					}
 				}
 			}
 		}
-	}		
+	}
 }
 
 print json_encode($result);
